c8c39fcbea58b4414710db2ffb13c1a8d059ea73
Brian O'Halloran <brian_o_halloran@hotmail.com>
2022-02-09 19:15:17 +0000

    Commenting out a few lines and amending resource class to use next gen and trying xcode test-without-building and adding a second test to show split

--- circleci-demo-macosTests/circleci_demo_macosTests.swift
+++ circleci-demo-macosTests/circleci_demo_macosTests.swift
@@ -18,3 +18,21 @@ class circleci_demo_macosTests: XCTestCase {
         XCTAssertTrue(timer.duration == 300)
     }
 }
+
+class circleci_demo_macosTests2: XCTestCase {
+
+    var timer:EggTimer!
+
+    override func setUp() {
+        timer = EggTimer()
+    }
+
+    override func tearDown() {
+        timer = nil
+    }
+
+    func testStartTime() {
+        // Expect the timer to be set at 5:00
+        XCTAssertTrue(timer.duration == 300)
+    }
+}
