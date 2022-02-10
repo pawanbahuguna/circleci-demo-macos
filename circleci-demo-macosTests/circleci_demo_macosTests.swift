8bc5b5ca2fcb0ecbc2191e8bddddd9ab334c5f1a
Brian O'Halloran <brian_o_halloran@hotmail.com>
2022-02-10 12:54:23 +0000

    Adding in test-only steps for specific .swift files

--- circleci-demo-macosTests/circleci_demo_macosTests.swift
+++ circleci-demo-macosTests/circleci_demo_macosTests.swift
@@ -19,20 +19,3 @@ class circleci_demo_macosTests: XCTestCase {
     }
 }
 
-class circleci_demo_macosTests2: XCTestCase {
-
-    var timer:EggTimer!
-
-    override func setUp() {
-        timer = EggTimer()
-    }
-
-    override func tearDown() {
-        timer = nil
-    }
-
-    func testStartTime() {
-        // Expect the timer to be set at 5:00
-        XCTAssertTrue(timer.duration == 300)
-    }
-}
--- circleci-demo-macosTests/circleci_demo_macosTests2.swift
+++ circleci-demo-macosTests/circleci_demo_macosTests2.swift
@@ -1,7 +1,7 @@
 import XCTest
 @testable import circleci_demo_macos2
 
-class circleci_demo_macosTests3: XCTestCase {
+class circleci_demo_macosTests2: XCTestCase {
     
     var timer:EggTimer!
 
@@ -19,20 +19,3 @@ class circleci_demo_macosTests3: XCTestCase {
     }
 }
 
-class circleci_demo_macosTests4: XCTestCase {
-
-    var timer:EggTimer!
-
-    override func setUp() {
-        timer = EggTimer()
-    }
-
-    override func tearDown() {
-        timer = nil
-    }
-
-    func testStartTime() {
-        // Expect the timer to be set at 5:00
-        XCTAssertTrue(timer.duration == 300)
-    }
-}
